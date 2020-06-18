f46f449ebd2b822b21dc9a1570ba606dc21d1434
Sharan777 <sharangouda021@gmail.com>
2020-06-18 20:32:52 +0530

    commit api response

--- src/app/dashboard/services/aws-cloud-serverless/trip-sheet.service.ts
+++ src/app/dashboard/services/aws-cloud-serverless/trip-sheet.service.ts
@@ -11,14 +11,14 @@ export class TripSheetService {
   getAllTripSheets() {
     this.http.get('https://7f5fars35b.execute-api.us-east-2.amazonaws.com/Dev/mill-crusher-api')
     .subscribe((result)=> {
-      console.log('from service' + result);
+      console.log('Service Data' + result );
     });
   }
 
   newTripSheet(formData) {
     this.http.post('https://7f5fars35b.execute-api.us-east-2.amazonaws.com/Dev/mill-crusher-api',formData)
     .subscribe((result)=> {
-      console.log('from service' + JSON.stringify(result))
+      console.log('Service Data' + JSON.stringify(result))
     });
   }
 }
